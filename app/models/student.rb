0e61e86ba74c312e60bb6c7158d99070dfce351f
Yomaira Escano <yjescano@gmail.com>
2018-03-06 03:41:48 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,12 +1,5 @@
 class Student < ActiveRecord::Base
-  # before_create :default_values
-
   def to_s
     self.first_name + " " + self.last_name
   end
-
-  # def default_values
-  #   self.active ||= false
-  # end
-
 end
